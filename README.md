7eff8503448dff204956f111096bc364d04dca98
Kozia <74949858+kozia132@users.noreply.github.com>
2025-07-26 23:08:13 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# hi
+you dont really need to read this lol
+
+simple ass boring ass mod that just removes the new game button on the title screen cause it can be scary sometimes
+
+uuuuhhh add UnityEngine, UnityEngine.CoreModule, and MelonLoader if u wanna use the sln or smth idk
+
+mostly made by chatgpt :thumbsup:
---
+++ remove.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using UnityEngine;
+using System.Collections;
+
+namespace DisableNewGameButton
+{
+    public class Main : MelonMod
+    {
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            MelonLogger.Msg($"Scene loaded: {sceneName}");
+
+            MelonCoroutines.Start(DisableNewGameButtonCoroutine());
+        }
+
+        private IEnumerator DisableNewGameButtonCoroutine()
+        {
+            yield return new WaitForSeconds(1f);
+
+            var buttonObj = GameObject.Find("/Main Menu/Canvas/Main Menu/Panel/Title Panel/Title Buttons/New Game Button");
+
+            if (buttonObj != null)
+            {
+                buttonObj.SetActive(false);
+                MelonLogger.Msg("new file button just got EPICLY NUKED");
+            }
+            else
+            {
+                MelonLogger.Warning("couldnt nuke new file button :( (not found)");
+            }
+        }
+    }
+}
